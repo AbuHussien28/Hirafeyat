1962783e47209d02fd00809ce1c7d16d318313c3
Ali Abu Hussisn <aliabuhussien@outlook.com>
2025-04-22 18:14:08 +0200

    Make Feature Login And Register

---
+++ Hirafeyat/ViewModel/Account/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace Hirafeyat.ViewModel.Account
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Full Name is required.")]
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.ImageUrl)]
+        public string imagePath { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage ="Address Is Required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Role is required.")]
+        public string Role { get; set; }
+    }
+}
