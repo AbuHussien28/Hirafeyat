47776bb740a8d60d185ad692391aab9d2e6847bd
Ali Abu Hussisn <aliabuhussien@outlook.com>
2025-05-03 22:36:43 +0300

    add quaity

---
+++ Hirafeyat/Migrations/20250503193020_QuentityAdded.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hirafeyat.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuentityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quentity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AccountCreatedDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quentity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "AccountCreatedDate",
+                table: "AspNetUsers");
+        }
+    }
+}
